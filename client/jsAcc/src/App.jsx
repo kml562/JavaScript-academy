624f63099e28709e98e5144c4899fd5bd741c2a3
Kamal Bisht <111355794+kml562@users.noreply.github.com>
2023-05-13 01:31:39 +0530

    done

--- client/jsAcc/src/App.jsx
+++ client/jsAcc/src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import AllRoutes from "./routes/AllRoutes";
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-     <h1>kml</h1>
+      <AllRoutes />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
