47b8447768d25ebaa81b8e611fd4cc2c3b329682
Kamal Bisht <111355794+kml562@users.noreply.github.com>
2023-05-14 21:49:39 +0530

    team page done

--- client/jsAcc/src/component/Navbar.jsx
+++ client/jsAcc/src/component/Navbar.jsx
@@ -106,7 +106,7 @@ export default function Navbar() {
                 <Avatar
                   size={"sm"}
                   src={
-                    "https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
+                    "https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg"
                   }
                 />
               </MenuButton>
@@ -161,7 +161,7 @@ _hover={{
   textDecoration: "none",
   bg: useColorModeValue("gray.200", "gray.700"),
 }}
-href={"#"}
+href={"/team"}
 >
 {"Team"}
 </Link>
