b21ed01fcba4f2007248635b154176018473d579
Kamal Bisht <111355794+kml562@users.noreply.github.com>
2023-05-14 16:02:06 +0530

    done

--- client/jsAcc/src/routes/AllRoutes.jsx
+++ client/jsAcc/src/routes/AllRoutes.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Homepage } from "../pages/Homepage";
 import Login from "../pages/Login";
+import Signup from "../pages/SignUp";
 
 const AllRoutes = () => {
   return (
@@ -9,6 +10,7 @@ const AllRoutes = () => {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={< Login />} />
+        <Route path="/signup" element={< Signup />} />
       </Routes>
     </div>
   );
